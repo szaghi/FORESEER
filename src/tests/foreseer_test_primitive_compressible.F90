f97b35017ec9d2d5f75cbdfb50a177c9e3317c93
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-03-20 11:33:48 +0100

    improve primitive object test coverage

--- src/tests/foreseer_test_primitive_compressible.F90
+++ src/tests/foreseer_test_primitive_compressible.F90
@@ -14,8 +14,11 @@ type(primitive_compressible)          :: another_p            !< A primitive com
 type(primitive_compressible), pointer :: p_pointer            !< A primitive compressible pointer.
 type(vector)                          :: momentum             !< Momentum vector.
 real(R8P), allocatable                :: p_serialized(:)      !< Primitive variable serialized.
+real(R8P), allocatable                :: l_eigenvectors(:,:)  !< Left eigenvectors matrix.
+real(R8P), allocatable                :: r_eigenvectors(:,:)  !< Right eigenvectors matrix.
+real(R8P), allocatable                :: identity(:,:)        !< Identity tensor.
 #ifdef __GFORTRAN__
-logical                               :: are_tests_passed(11) !< List of passed tests.
+logical                               :: are_tests_passed(15) !< List of passed tests.
 #else
 logical                               :: are_tests_passed(7)  !< List of passed tests.
 #endif
@@ -92,7 +95,51 @@ are_tests_passed(11) = (p%density  >= 0._R8P - ZeroR8).and.(p%density  <= 0._R8P
                        (p%velocity >= 0._R8P - ZeroR8).and.(p%velocity <= 0._R8P + ZeroR8).and. &
                        (p%pressure >= 0._R8P - ZeroR8).and.(p%pressure <= 0._R8P + ZeroR8)
 print "(A,L1)", 'p - p, is done right? ', are_tests_passed(11)
+
+p = primitive_compressible(density=1._R8P, pressure=1._R8P)
+
+another_p = - p
+are_tests_passed(12) = (another_p%density  >= -1._R8P - ZeroR8).and.(another_p%density  <= -1._R8P + ZeroR8).and. &
+                       (another_p%pressure >= -1._R8P - ZeroR8).and.(another_p%pressure <= -1._R8P + ZeroR8)
+print "(A,L1)", 'another_p = - p, is done right? ', are_tests_passed(12)
+
+another_p = + p
+are_tests_passed(13) = (another_p%density  >= 1._R8P - ZeroR8).and.(another_p%density  <= 1._R8P + ZeroR8).and. &
+                       (another_p%pressure >= 1._R8P - ZeroR8).and.(another_p%pressure <= 1._R8P + ZeroR8)
+print "(A,L1)", 'another_p = + p, is done right? ', are_tests_passed(13)
+
+p = p * 2._R8P
+are_tests_passed(14) = (p%density  >= 2._R8P - ZeroR8).and.(p%density  <= 2._R8P + ZeroR8).and. &
+                       (p%pressure >= 2._R8P - ZeroR8).and.(p%pressure <= 2._R8P + ZeroR8)
+print "(A,L1)", 'p * 2, is done right? ', are_tests_passed(14)
+
+p = p / 2._R8P
+are_tests_passed(15) = (p%density  >= 1._R8P - ZeroR8).and.(p%density  <= 1._R8P + ZeroR8).and. &
+                       (p%pressure >= 1._R8P - ZeroR8).and.(p%pressure <= 1._R8P + ZeroR8)
+print "(A,L1)", 'p / 2, is done right? ', are_tests_passed(15)
 #endif
+
+p = primitive_compressible(density=1._R8P, velocity=ex, pressure=1._R8P)
+
+print "(A)", "Test pretty printing"
+print "(A)", p_pointer%description()
+
+print "(A)", "Test eigenvector computing"
+l_eigenvectors = p%left_eigenvectors(eos=eos)
+r_eigenvectors = p%right_eigenvectors(eos=eos)
+identity = matmul(l_eigenvectors, r_eigenvectors)
+print "(A)", "Left"
+print "(3F7.3)", l_eigenvectors(1,:)
+print "(3F7.3)", l_eigenvectors(2,:)
+print "(3F7.3)", l_eigenvectors(3,:)
+print "(A)", "Right"
+print "(3F7.3)", r_eigenvectors(1,:)
+print "(3F7.3)", r_eigenvectors(2,:)
+print "(3F7.3)", r_eigenvectors(3,:)
+print "(A)", "L * R"
+print "(3F7.3)", identity(1,:)
+print "(3F7.3)", identity(2,:)
+print "(3F7.3)", identity(3,:)
 
 print "(A,L1)", new_line('a')//'Are all tests passed? ', all(are_tests_passed)
 endprogram foreseer_test_primitive_compressible
