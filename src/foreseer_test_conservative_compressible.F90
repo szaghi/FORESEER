db01b17745396db4503a5914d86d688ff05b0de2
Stefano Zaghi <stefano.zaghi@gmail.com>
2017-03-21 17:45:38 +0000

    Travis CI autocommit from travis build 18

--- src/foreseer_test_conservative_compressible.F90
+++ src/foreseer_test_conservative_compressible.F90
@@ -10,14 +10,15 @@ use vecfor, only : ex, vector
 implicit none
 type(eos_compressible)                   :: eos                  !< An equation of state.
 type(conservative_compressible)          :: u                    !< A conservative compressible instance.
+type(conservative_compressible)          :: another_u            !< Another conservative compressible instance.
 type(conservative_compressible), pointer :: u_pointer            !< A conservative compressible pointer.
 type(conservative_compressible)          :: f                    !< Conservative fluxes.
 type(vector)                             :: velocity             !< Velocity vector.
 real(R8P), allocatable                   :: u_serialized(:)      !< Conservative variable serialized.
 #ifdef __GFORTRAN__
-logical                                  :: are_tests_passed(16) !< List of passed tests.
+logical                                  :: are_tests_passed(17) !< List of passed tests.
 #else
-logical                                  :: are_tests_passed(8)  !< List of passed tests.
+logical                                  :: are_tests_passed(9)  !< List of passed tests.
 #endif
 
 are_tests_passed = .false.
@@ -76,53 +77,59 @@ are_tests_passed(8) = (u_pointer%density  >= 1._R8P -  ZeroR8).and.(u_pointer%de
                       (u_pointer%energy   >= 2.5_R8P - ZeroR8).and.(u_pointer%energy   <= 2.5_R8P + ZeroR8)
 print "(A,L1)", 'u => u, is done right? ', are_tests_passed(8)
 
+call another_u%initialize(initial_state=u)
+are_tests_passed(9) = (another_u%density  >= 1._R8P -  ZeroR8).and.(another_u%density  <= 1._R8P  + ZeroR8).and. &
+                      (another_u%momentum >= 1._R8P -  ZeroR8).and.(another_u%momentum <= 1._R8P  + ZeroR8).and. &
+                      (another_u%energy   >= 2.5_R8P - ZeroR8).and.(another_u%energy   <= 2.5_R8P + ZeroR8)
+print "(A,L1)", 'antoher_u == u, is right? ', are_tests_passed(9)
+
 #ifdef __GFORTRAN__
 u = conservative_compressible(density=1._R8P, momentum=ex, energy=2.5_R8P)
 u = 2._R8P * u
-are_tests_passed(9) = (u%density  >= 2._R8P - ZeroR8).and.(u%density  <= 2._R8P + ZeroR8).and. &
-                      (u%momentum >= 2._R8P - ZeroR8).and.(u%momentum <= 2._R8P + ZeroR8).and. &
-                      (u%energy   >= 5._R8P - ZeroR8).and.(u%energy   <= 5._R8P + ZeroR8)
-print "(A,L1)", '2 * u, is done right? ', are_tests_passed(9)
+are_tests_passed(10) = (u%density  >= 2._R8P - ZeroR8).and.(u%density  <= 2._R8P + ZeroR8).and. &
+                       (u%momentum >= 2._R8P - ZeroR8).and.(u%momentum <= 2._R8P + ZeroR8).and. &
+                       (u%energy   >= 5._R8P - ZeroR8).and.(u%energy   <= 5._R8P + ZeroR8)
+print "(A,L1)", '2 * u, is done right? ', are_tests_passed(10)
 
 u = u * u
-are_tests_passed(10) = (u%density  >= 4._R8P -  ZeroR8).and.(u%density  <= 4._R8P  + ZeroR8).and. &
+are_tests_passed(11) = (u%density  >= 4._R8P -  ZeroR8).and.(u%density  <= 4._R8P  + ZeroR8).and. &
                        (u%momentum >= 4._R8P -  ZeroR8).and.(u%momentum <= 4._R8P  + ZeroR8).and. &
                        (u%energy   >= 25._R8P - ZeroR8).and.(u%energy   <= 25._R8P + ZeroR8)
-print "(A,L1)", 'u * u, is done right? ', are_tests_passed(10)
+print "(A,L1)", 'u * u, is done right? ', are_tests_passed(11)
 
 u = u + u
-are_tests_passed(11) = (u%density  >= 8._R8P -  ZeroR8).and.(u%density  <= 8._R8P  + ZeroR8).and. &
+are_tests_passed(12) = (u%density  >= 8._R8P -  ZeroR8).and.(u%density  <= 8._R8P  + ZeroR8).and. &
                        (u%momentum >= 8._R8P -  ZeroR8).and.(u%momentum <= 8._R8P  + ZeroR8).and. &
                        (u%energy   >= 50._R8P - ZeroR8).and.(u%energy   <= 50._R8P + ZeroR8)
-print "(A,L1)", 'u + u, is done right? ', are_tests_passed(11)
+print "(A,L1)", 'u + u, is done right? ', are_tests_passed(12)
 
 u = u - u
-are_tests_passed(12) = (u%density  >= 0._R8P - ZeroR8).and.(u%density  <= 0._R8P + ZeroR8).and. &
+are_tests_passed(13) = (u%density  >= 0._R8P - ZeroR8).and.(u%density  <= 0._R8P + ZeroR8).and. &
                        (u%momentum >= 0._R8P - ZeroR8).and.(u%momentum <= 0._R8P + ZeroR8).and. &
                        (u%energy   >= 0._R8P - ZeroR8).and.(u%energy   <= 0._R8P + ZeroR8)
-print "(A,L1)", 'u - u, is done right? ', are_tests_passed(12)
+print "(A,L1)", 'u - u, is done right? ', are_tests_passed(13)
 
 u = conservative_compressible(density=1._R8P, energy=2.5_R8P)
 
 u = - u
-are_tests_passed(13) = (u%density >= -1._R8P  - ZeroR8).and.(u%density <= -1._R8P  + ZeroR8).and. &
-                       (u%energy  >= -2.5_R8P - ZeroR8).and.(u%energy  <= -2.5_R8P + ZeroR8)
-print "(A,L1)", 'u = - u, is done right? ', are_tests_passed(12)
-
-u = + u
 are_tests_passed(14) = (u%density >= -1._R8P  - ZeroR8).and.(u%density <= -1._R8P  + ZeroR8).and. &
                        (u%energy  >= -2.5_R8P - ZeroR8).and.(u%energy  <= -2.5_R8P + ZeroR8)
-print "(A,L1)", 'u = + u, is done right? ', are_tests_passed(14)
+print "(A,L1)", 'u = - u, is done right? ', are_tests_passed(14)
+
+u = + u
+are_tests_passed(15) = (u%density >= -1._R8P  - ZeroR8).and.(u%density <= -1._R8P  + ZeroR8).and. &
+                       (u%energy  >= -2.5_R8P - ZeroR8).and.(u%energy  <= -2.5_R8P + ZeroR8)
+print "(A,L1)", 'u = + u, is done right? ', are_tests_passed(15)
 
 u = u * 2._R8P
-are_tests_passed(15) = (u%density >= -2._R8P - ZeroR8).and.(u%density <= -2._R8P + ZeroR8).and. &
+are_tests_passed(16) = (u%density >= -2._R8P - ZeroR8).and.(u%density <= -2._R8P + ZeroR8).and. &
                        (u%energy  >= -5._R8P - ZeroR8).and.(u%energy  <= -5._R8P + ZeroR8)
-print "(A,L1)", 'u * 2, is done right? ', are_tests_passed(15)
+print "(A,L1)", 'u * 2, is done right? ', are_tests_passed(16)
 
 u = u / 2._R8P
-are_tests_passed(16) = (u%density >= -1._R8P  - ZeroR8).and.(u%density <= -1._R8P  + ZeroR8).and. &
+are_tests_passed(17) = (u%density >= -1._R8P  - ZeroR8).and.(u%density <= -1._R8P  + ZeroR8).and. &
                        (u%energy  >= -2.5_R8P - ZeroR8).and.(u%energy  <= -2.5_R8P + ZeroR8)
-print "(A,L1)", 'u / 2, is done right? ', are_tests_passed(16)
+print "(A,L1)", 'u / 2, is done right? ', are_tests_passed(17)
 #endif
 
 print "(A,L1)", new_line('a')//'Are all tests passed? ', all(are_tests_passed)
